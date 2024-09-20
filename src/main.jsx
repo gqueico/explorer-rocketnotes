9fdd681b980a80b928b7dea9d34b0d100a00106f
gqueico <gqueico@gmail.com>
2024-09-19 22:19:33 -0300

    :tada: Start project

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global'
+import theme from './styles/theme'
+
+import { Details } from './pages/Details'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider theme = {theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
+  </StrictMode>
+)
